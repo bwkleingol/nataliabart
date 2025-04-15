bc407efb97fe60ca0ad586be380a09cd6914ebd1
Ruben Dinkelman <ruben@dinkelworks.nl>
2025-04-15 19:02:58 +0200

    fix build

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import astroI18next from 'astro-i18next';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bartnathaliawedding.com',
   compressHTML: true,
+  output: 'server',
+  adapter: vercel(),
   build: {
     assets: '_assets'
   },
